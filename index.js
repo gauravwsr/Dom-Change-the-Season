5a64cbdbd8b79178b0b7b9d69dfb394bafe7896c
gaurav <gauravwasekar7@gmail.com>
2024-10-25 13:46:05 +0530

    All files added

---
+++ index.js
@@ -0,0 +1,18 @@
+let Springbtn = document.getElementById("Spring");
+let Summerbtn = document.getElementById("Summer");
+let Autumnbtn = document.getElementById("Autumn");
+let Winterbtn = document.getElementById("Winter");
+let img = document.getElementById("img");
+
+function Spring(){
+    img.src="img/Spring.jpg";
+}
+function Summer(){
+    img.src="img/Summer.jpg";
+}
+function Autumn(){
+    img.src="img/Autumn.jpg";
+}
+function Winter(){
+    img.src="img/Winter.jpg";
+}
